b9fa0ae0ede1f9e892e1aa042a2ceedceda7c213
venkateswaran95 <95venkateswaran@gmail.com>
2020-04-14 11:16:20 +0530

    changes

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -11,6 +11,7 @@ sap.ui.define([
 			var sPath = jQuery.sap.getModulePath("code.appUI5_Hello", "/data/data.json"); 
 			var oModel = new JSONModel(sPath);
 			this.getView().setModel(oModel);
+			this.oTable = this.getView().byId("table1");    
 		},
 		onNavBack: function(){
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
@@ -21,15 +22,25 @@ sap.ui.define([
 		},
 		onSearch: function(){
 			var comboBoxValue=this.byId("oComboBox").getValue();
-			var oTable = this.getView().byId("table1");    
-			var obinding=oTable.getBinding("items"), oFilter;
+			
+			var obinding=this.oTable.getBinding("items"), oFilter;
 			if(comboBoxValue || comboBoxValue === ""){
 				oFilter= new Filter("location","EQ",comboBoxValue);
 				obinding.filter([oFilter]);
 			}
 		},
 		onReset: function(){
-			
+			var obinding=this.oTable.getBinding("items");
+			obinding.filter([]);
+			this.byId("oComboBox").setSelectedItem(null);
+		},
+		handleListItem : function(oEvent){
+			var location = oEvent.getSource().getBindingContext().getProperty("location");
+			var obinding=this.oTable.getBinding("items"), oFilter;
+			//if(comboBoxValue || comboBoxValue === ""){
+				oFilter= new Filter("location","EQ",location);
+				obinding.filter([oFilter]);
+			//}
 		}
 			
 	});	
